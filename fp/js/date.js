e5dc15d5a34556f09e638a7465947ab6d0ad52bf
Rhythm-J <39495199+Rhythm-J@users.noreply.github.com>
2020-02-22 19:53:03 +0530

    date limitations

---
+++ fp/js/date.js
@@ -0,0 +1,14 @@
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+ if(dd<10){
+        dd='0'+dd
+    }
+    if(mm<10){
+        mm='0'+mm
+    }
+
+today = dd+'-'+mm+'-'+yyyy;
+document.getElementById("appdate").setAttribute("min", today);
+document.getElementById("dob").setAttribute("max", today);
