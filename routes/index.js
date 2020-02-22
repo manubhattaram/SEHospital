47ae6de7c2eaabb291006be36ef64268a08070a1
tanuja3 <dtanujakirthi@gmail.com>
2020-02-23 01:12:07 +0530

    Appointment

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,44 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET New App page. */
+router.get('/newapp', function(req, res) {
+    res.render('newapp', { title: 'New App' });
+});
+
+/*POST to appointment service*/
+router.post('/appointment' , function(req,res){
+	// Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var name = req.body.name;
+    var email = req.body.email;
+    var dob = req.body.dob;
+    var field = req.body.field;
+    var appdate = req.body.appdate;
+    var message = req.body.message;
+    
+    // Set our collection
+    var appointment = db.get('appointments');
+
+    // Submit to the DB
+    appointment.insert({
+        "name" : name,
+        "email" : email,
+        "dob" : dob,
+        "field" : field,
+        "appdate" : appdate,
+        "message" : message
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And redirect back to main page
+            res.redirect('/');
+        }
+    });
+});
 module.exports = router;
