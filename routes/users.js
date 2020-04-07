07929106156a30468f3b477c4e936f0dbd406a31
manubhattaram <54678843+manubhattaram@users.noreply.github.com>
2020-04-07 20:54:55 +0530

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 //Patient Login Page
-router.get('/patientlogin', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/patientlogin', (req, res)=> res.render('patientlogin'));
 
 //Staff Login Page
-router.get('/stafflogin', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/stafflogin', (req, res)=> res.render('stafflogin'));
 
 /*//Register Page
 router.get('/register', function(req, res, next) {
