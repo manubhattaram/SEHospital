4af7e2d02c68860cf843d3aa2796c41c70e90a7e
Rhythm-J <39495199+Rhythm-J@users.noreply.github.com>
2020-06-17 11:36:26 +0530

    minor changes

--- bin/mailing.py
+++ bin/mailing.py
@@ -1,17 +1,17 @@
-    
+
 import smtplib, ssl, sys
 
 string1 = sys.argv[2];
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
 sender_email = "hestia.medical.help@gmail.com"
-rcvr_email = "tanujakirthid@gmail.com"
-password = "hestia123" 
+rcvr_email = sys.argv[1]
+password = "hestia123"
 message = """\
 Subject: About Your Appointment at Hestia Medical.
 
 """
-message = message+string1+sys.argv[1];
+message = message+string1
 print(message)
 
 #input("Type your password and press enter: ")
@@ -22,9 +22,9 @@ context = ssl.create_default_context()
 # Try to log in to server and send email
 try:
 	server = smtplib.SMTP(smtp_server,port)
-	server.ehlo() 
+	server.ehlo()
 	server.starttls(context=context) # Secure the connection
-	server.ehlo() 
+	server.ehlo()
 	server.login(sender_email, password)
 	server.sendmail(sender_email, rcvr_email, message)
 
@@ -32,4 +32,4 @@ except Exception as e:
     print("Error: ")
     print(e)
 finally:
-    server.quit() 
+    server.quit()
